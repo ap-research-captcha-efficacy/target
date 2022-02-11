0b6100ca9cf57e8e527167485b8c5a00ff1915f6
cynic <kurisufag1@gmail.com>
2022-02-11 08:07:44 -0500

    clean updebug lines

--- target/types/test-target.py
+++ target/types/test-target.py
@@ -4,5 +4,4 @@ from secrets import token_hex
 # the solver needs to brute the 4 char hash, good for testing time to solve and a testing suite
 def generate(mod):
     key = token_hex(3)
-    print(key)
     return ("data:,"+sha256(key.encode("ascii")).hexdigest(), key)
