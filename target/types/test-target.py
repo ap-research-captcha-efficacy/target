5f4a1d64c29407a0f136f8df2b343edd75a4c941
cynic <kurisufag1@gmail.com>
2022-02-11 08:03:21 -0500

    reset captcha expiry time to one minute

--- target/types/test-target.py
+++ target/types/test-target.py
@@ -1,7 +1,8 @@
 from hashlib import sha256
 from secrets import token_hex
 
-# the solver needs to brute the 5 char hash, good for testing time to solve and a testing suite
+# the solver needs to brute the 4 char hash, good for testing time to solve and a testing suite
 def generate(mod):
-    key = token_hex(5)
+    key = token_hex(3)
+    print(key)
     return ("data:,"+sha256(key.encode("ascii")).hexdigest(), key)
