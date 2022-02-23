a79a51b0e628733622729c861d375b7c992d0ef8
cynic <kurisufag1@gmail.com>
2022-02-23 07:32:47 -0500

    weaken test-target

--- target/types/test-target.py
+++ target/types/test-target.py
@@ -1,7 +1,7 @@
 from hashlib import sha256
 from secrets import token_hex
 
-# the solver needs to brute the 4 char hash, good for testing time to solve and a testing suite
+# the solver needs to brute the hash, good for testing time to solve and a testing suite
 def generate(mod):
-    key = token_hex(3)
+    key = token_hex(2)
     return ("data:,"+sha256(key.encode("ascii")).hexdigest(), key)
