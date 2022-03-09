b91e612356f5a2426c14b704c7bde87b70b76952
cynic <kurisufag1@gmail.com>
2022-03-09 18:28:31 -0500

    add line obstructions to basic

--- target/types/basic.py
+++ target/types/basic.py
@@ -2,16 +2,23 @@ from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 from base64 import b64encode
 from secrets import token_hex
+from random import randint
+
+valid_mods = ["obstruct"]
 
 def generate(mod):
+    print(mod)
     key = token_hex(3)
 
     data = BytesIO()
-
     img = Image.new(mode="RGB", size=(300, 100), color=(255, 255, 255))
     id = ImageDraw.Draw(img)
+    if mod and "obstruct" in mod:
+        for i in range(0, 10):
+            print("WE OUT HERE")
+            id.line((randint(0, 300), randint(0, 100), randint(0, 300), randint(0, 100)), fill=128)
     font_large = ImageFont.truetype("fonts/DejaVuSansMono.ttf", size=20)
-    id.text((0, 0), key, font=font_large, fill=(55, 55, 55))
+    id.text((120+randint(-20,20), 40+randint(-20,20)), key, font=font_large, fill=(55, 55, 55))
     
     img.save(data, format="PNG")
     data.seek(0)
