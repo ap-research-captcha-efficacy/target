60e45791b584c347145a82333a2b78046e06d4af
cynic <kurisufag1@gmail.com>
2022-03-17 21:50:02 -0400

    hinting

--- target/types/basic.py
+++ target/types/basic.py
@@ -15,10 +15,14 @@ def generate(mod):
     id = ImageDraw.Draw(img)
     if mod and "obstruct" in mod:
         for i in range(0, 10):
-            id.line((randint(0, 300), randint(0, 100), randint(0, 300), randint(0, 100)), fill=128)
+            id.line((randint(1, 300), randint(1, 100), randint(1, 300), randint(1, 100)), fill=128)
     font_large = ImageFont.truetype("fonts/DejaVuSansMono.ttf", size=20)
-    id.text((120+randint(-20,20), 40+randint(-20,20)), key, font=font_large, fill=(0, 0, 0))
+    x, y = (120+(randint(-2,2)*10), 40+(randint(-2,2)*10))
+    id.text((x, y), key, font=font_large, fill=(0, 0, 0))
     
+    img.putpixel((0, y), 0)
+    img.putpixel((x, 0), 0)
+
     img.save(data, format="PNG")
     data.seek(0)
     return ("data:image/png;base64,"+b64encode(data.getvalue()).decode("ascii"), key)
